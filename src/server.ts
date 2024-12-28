5df765c06dde6fee8c9fd53ed829f0a575cc857c
Vishal Mishra <vm174294@gmail.com>
2024-12-28 13:56:10 +0530

    cors error fixed

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ dotenv.config({});
 app.use(express.json())
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://hd-note-app.vercel.app/",
+    origin: "https://hd-note-app.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE' ],
     allowedHeaders: ['Content-Type', 'Authorization'],
